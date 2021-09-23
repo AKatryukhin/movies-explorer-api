0fb89d83ea18286efddc698198e9436ff6e300d6
AKatryukhin <A.Katryukhin@yandex.ru>
2021-09-23 09:19:32 +0300

    refactor: edit users controllers

--- controllers/users.js
+++ controllers/users.js
@@ -42,6 +42,7 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
+          sameSite: true,
         })
         .send({
           _id: user._id,
